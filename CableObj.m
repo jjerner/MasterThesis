0afcd0d92c4140f1781dd248158a1319f3c95ae7
Jerner <jonathan.jerner@gmail.com>
2018-10-01 13:15:59 +0200

    First commit

---
+++ CableObj.m
@@ -0,0 +1,42 @@
+classdef CableObj
+    properties
+        Length;                         %% l [m]
+        Diameter;                       %% d [m]
+        SeriesResistance;               %% R [Ohm / m]
+        ShuntConductance;               %% G [S (1/Ohm)]
+        SeriesInductance;               %% L [H/m]
+        ShuntCapacitance;               %% C [F/m]
+        Impedance;                      %% z = R + jwL
+        Admittance;                     %% y = G + jwC
+    end
+
+    methods
+        
+        function obj = init(obj,l, d, R, G, L, C)
+            if nargin < 7
+                error('Missing input parameter');
+            elseif nargin > 7
+                error('Too many input parameters');
+            else
+                obj.Length = l;                         %% l [m]
+                obj.Diameter = d;                       %% d [m]
+                obj.SeriesResistance = R;               %% R [Ohm / m]
+                obj.ShuntConductance = G;               %% G [S (1/Ohm)]
+                obj.SeriesInductance = L;               %% L [H/m]
+                obj.ShuntCapacitance = C;               %% C [F/m]
+            end
+        end
+        
+        function obj = impedanceCalc(obj, phase)
+            R = obj.SeriesResistance;
+            L = obj.SeriesInductance;
+            j = 1i;
+            w = phase;
+            obj.Impedance = R + j*w*L;
+        end
+        
+        function obj = adittanceCalc(obj, phase)
+            
+        end
+    end
+end
---
+++ main.m
@@ -0,0 +1,6 @@
+clear all
+clc
+
+a = CableObj;
+a = init(a, 1,2,3,4,5,6);
+a = impedanceCalc(a, 1);

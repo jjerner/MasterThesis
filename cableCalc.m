22e4a1e8109cd3149d2bd4575550d6eff25757d3
lbs129 <lbs129@hotmail.com>
2018-10-26 14:25:29 +0200

    Update cableCalc.m
    Commented code and renamed dataStruct to CableData.

--- cableCalc.m
+++ cableCalc.m
@@ -1,15 +1,27 @@
-function [V_S,I_S]=cableCalc(V_R,I_R,dataStruct,cableID)
+function [V_S,I_S]=cableCalc(V_R,I_R,CableData,cableID)
+% Calculates voltage and current at sending end of cable for specified
+% voltage and current at receiving end.
+% 
+% Inputs:
+% V_R: Voltage at receiving end [V]
+% I_R: Current at receiving end [A]
+% CableData: Struct containing cable parameters.
+% cableID: Integer specifying which cable in the struct.
+%
+% Outputs:
+% V_S: Voltage at sending end [V]
+% I_S: Current at sending end [A]
 
 % Voltage, sending end
-V_inc=(V_R+dataStruct(cableID).Z_C*I_R)...
-    *exp(dataStruct(cableID).gamma.*dataStruct(cableID).l)/2;        % Incident voltage [V]
-V_ref=(V_R-dataStruct(cableID).Z_C*I_R)...
-    *exp(-dataStruct(cableID).gamma.*dataStruct(cableID).l)/2;       % Reflected voltage [V]
+V_inc=(V_R+CableData(cableID).Z_C*I_R)...
+    *exp(CableData(cableID).gamma.*CableData(cableID).l)/2;        % Incident voltage [V]
+V_ref=(V_R-CableData(cableID).Z_C*I_R)...
+    *exp(-CableData(cableID).gamma.*CableData(cableID).l)/2;       % Reflected voltage [V]
 V_S=V_inc+V_ref;                                                   % Sending voltage [V]
 
 % Current, sending end
-I_S=((V_R./dataStruct(cableID).Z_C+I_R)...
-    *exp(dataStruct(cableID).gamma.*dataStruct(cableID).l)/2)...
-    -((V_R./dataStruct(cableID).Z_C-I_R)...
-    *exp(-dataStruct(cableID).gamma.*dataStruct(cableID).l)/2);      % Sending current [A]
+I_S=((V_R./CableData(cableID).Z_C+I_R)...
+    *exp(CableData(cableID).gamma.*CableData(cableID).l)/2)...
+    -((V_R./CableData(cableID).Z_C-I_R)...
+    *exp(-CableData(cableID).gamma.*CableData(cableID).l)/2);      % Sending current [A]
 end

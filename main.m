d659fad12a02d1665a4b35e9bd5d3f10a4ffc4af
Jerner <jonathan.jerner@gmail.com>
2018-10-29 13:26:17 +0100

    Update main.m

--- main.m
+++ main.m
@@ -4,6 +4,7 @@ addpath "PV model"
 %% Initialize parameters
 freq = 50;                      % frequency
 w = 2*pi*freq;                  % omeeeeeeeeeeega
+j = 1i;
 
 InitializeTransformer;         	% transformer parameters
 InitializeCables;               % cable parameters

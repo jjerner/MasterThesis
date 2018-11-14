605eece8baf786eb81cc0317d1ecfba96f20661d
lbs129 <lbs129@hotmail.com>
2018-11-14 10:35:51 +0100

    Update solveGS.m

--- solveGS.m
+++ solveGS.m
@@ -10,6 +10,7 @@ Q_inj=ones(1,length(Y_bus));        % Reactive power injected at each node [VAr]
 S_inj=Q_inj+j*Q_inj;                % Apparent power injected at each node [VA]
 
 V_latest=V_0;
+P_latest=zeros(1,length(Y_bus));
 V_hist(1,:)=V_0;
 iLoop=1;
 
@@ -21,11 +22,18 @@ for iLoop=1:10                      % Test
             -sum(Y_bus(iBus,:).*V_latest));
         
         % if PV-bus, find Q
+        Q_latest(iBus)=real(conj(V_latest(iBus))*V_latest(iBus)...
+            *sum(Y_bus(iBus,:))-sum(Y_bus(iBus,:).*V_latest));
         
         % if slack-bus, find P
-        
+        P_latest(iBus)=-imag(conj(V_latest(iBus))*V_latest(iBus)...
+            *sum(Y_bus(iBus,:))-sum(Y_bus(iBus,:).*V_latest));
     end
     V_hist(iLoop+1,:)=V_latest;
+    P_hist(iLoop+1,:)=P_latest;
+    Q_hist(iLoop+1,:)=Q_latest;
     V_diff=V_hist(iLoop+1,:)-V_hist(iLoop,:);
+    P_diff=V_hist(iLoop+1,:)-V_hist(iLoop,:);
+    Q_diff=V_hist(iLoop+1,:)-V_hist(iLoop,:);
 end
 

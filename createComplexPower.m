d1e8cf834b32ae78c38cd6d9c06a3b4fe047941e
lbs129 <lbs129@hotmail.com>
2018-12-12 14:47:35 +0100

    Possible to set leading mode in createComplexPower

--- createComplexPower.m
+++ createComplexPower.m
@@ -1,18 +1,26 @@
 % Create complex power from magnitude and power factor
 %
-% function S = createComplexPower(S_abs,cosPhi)
+% function S = createComplexPower(S_abs,powerFactor,leading)
 %
 % INPUTS:
-% S_abs:    Power magnitude (absolute value) [any matrix]
-% cosPhi:   Power factor cos(phi) [scalar or size(S_abs)]
+% S_abs:        Power magnitude (absolute value) [any matrix]
+% powerFactor:  Power factor cos(phi) [1x1 or size(S_abs)]
+% leading:      Make power factor leading (capacitive) instead of
+%               lagging (inductive). Default: off.
+%               [optional logical, 1x1 or size(S_abs)]
 %
 % OUTPUTS:
-% S:        Complex power [size(S_abs)]
+% S:            Complex power [size(S_abs)]
 
-function S = createComplexPower(S_abs,cosPhi)
-    j=1i;
-    S_abs=abs(S_abs);                % Make sure S_abs is magnitude
-    P=S_abs.*cosPhi;                 % Active power
-    Q=S_abs.*sin(acos(cosPhi));      % Reactive power
-    S=P+j.*Q;                        % Complex power
+function S = createComplexPower(S_abs,powerFactor,leading)
+    if ~exist('leading','var'), leading = 0; end    % Leading mode is off by default
+    j=1i;                                   % Imaginary unit
+    S_abs=abs(S_abs);                       % Make sure S_abs is magnitude
+    P=S_abs.*powerFactor;                   % Active power
+    if ~leading
+        Q=S_abs.*sin(acos(powerFactor));    % Reactive power (positive)
+    elseif leading
+        Q=-S_abs.*sin(acos(powerFactor));   % Reactive power (negative)
+    end
+    S=P+j.*Q;                               % Complex power
 end

a9247f56ec7e2dbf335fecfb9c201e277ea84443
jjerner <jerner91@gmail.com>
2018-12-18 15:35:35 +0100

    fixed input reading

---
+++ Verification/ELVinputs.m
@@ -0,0 +1,12 @@
+
+LPfolder = 'European_LV_CSV/Load Profiles';
+
+for indexLoad = 1:height(Loads)
+    loadShape = Loads.Yearly(indexLoad);
+    
+    filename = ['Load_Profile_', num2str(loadShape), '.csv'];
+    LPimport = ELVLoadImport(filename);     % read data from filename --> output in kW
+
+    Input(indexLoad).name = Loads.Yearly(indexLoad);
+    Input(indexLoad). values = LPimport;
+end

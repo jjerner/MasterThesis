c2c1773aa4dc29af4074f58a85cabc7f82977a8d
Jerner <jonathan.jerner@gmail.com>
2018-12-19 16:10:11 +0100

    Update verificationMain.m

--- Verification/verificationMain.m
+++ Verification/verificationMain.m
@@ -18,3 +18,14 @@ SetupArrays;
 % inputs
 ELVinputs;
 verificationProblem;
+
+pf = 0.95;
+
+S_complex = createComplexPower(S_bus, pf, 0);
+
+[U_hist,S_hist]=doSweepCalcs(Z_ser_tot, S_complex, U_bus, connectionBuses, busType, 1:1440);
+
+plot(1:1440, real(S_hist(2,:).*TransformerData.S_base))
+hold on
+plot(1:1440, imag(S_hist(2,:).*TransformerData.S_base))
+

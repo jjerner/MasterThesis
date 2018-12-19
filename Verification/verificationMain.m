aa1cf8d41031f3dad24d0a313dde5f3a73bd580a
Jerner <jonathan.jerner@gmail.com>
2018-12-19 16:54:03 +0100

    Update verificationMain.m

--- Verification/verificationMain.m
+++ Verification/verificationMain.m
@@ -25,17 +25,18 @@ S_complex = createComplexPower(S_bus, pf, 0);
 
 [U_hist,S_hist]=doSweepCalcs(Z_ser_tot, S_complex, U_bus, connectionBuses, busType, 1:1440);
 
-
-% plot Transformer hv side
-figure;
-plot(1:1440, real(S_hist(1,:).*TransformerData.S_base))
-hold on
-plot(1:1440, imag(S_hist(1,:).*TransformerData.S_base))
-hold off
-
 % plot Transformer lv side
 figure;
 plot(1:1440, real(S_hist(2,:).*TransformerData.S_base))
 hold on
 plot(1:1440, imag(S_hist(2,:).*TransformerData.S_base))
+hold off
+
+% voltages
+figure;
+plot(1:1440, abs(U_hist(35,:).*TransformerData.U_sec_base./sqrt(3)), 'b');  % load1
+hold on
+plot(1:1440, abs(U_hist(615,:).*TransformerData.U_sec_base./sqrt(3)), 'r'); % load32
+hold on
+plot(1:1440, abs(U_hist(900,:).*TransformerData.U_sec_base./sqrt(3)), 'g'); % load53
 hold off

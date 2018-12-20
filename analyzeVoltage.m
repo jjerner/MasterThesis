5e74df2085544728d954e54d3f60a51749f324d2
lbs129 <lbs129@hotmail.com>
2018-12-20 11:14:12 +0100

    Small fixes

--- analyzeVoltage.m
+++ analyzeVoltage.m
@@ -21,10 +21,10 @@ function U_anaRes=analyzeVoltage(U_hist,busIsLoad)
     maxLoadVoltageTimeStep=maxVoltageTimeVec(maxLoadVoltageBusNr);     % Time (col) for max load voltage
     minLoadVoltageTimeStep=minVoltageTimeVec(minLoadVoltageBusNr);     % Time (col) for min load voltage
 
-    fprintf('Maximum voltage: %g at (%d,%d)\n',maxVoltage,maxVoltageBusNr,maxVoltageTimeStep);
-    fprintf('Minimum voltage: %g at (%d,%d)\n',minVoltage,minVoltageBusNr,minVoltageTimeStep);
-    fprintf('Maximum load voltage: %g at (%d,%d)\n',maxLoadVoltage,maxLoadVoltageBusNr,maxLoadVoltageTimeStep);
-    fprintf('Minimum load voltage: %g at (%d,%d)\n',minLoadVoltage,minLoadVoltageBusNr,minLoadVoltageTimeStep);
+    fprintf('Maximum voltage: %g at bus %d, time %d\n',maxVoltage,maxVoltageBusNr,maxVoltageTimeStep);
+    fprintf('Minimum voltage: %g at bus %d, time %d\n',minVoltage,minVoltageBusNr,minVoltageTimeStep);
+    fprintf('Maximum load voltage: %g at bus %d, time %d\n',maxLoadVoltage,maxLoadVoltageBusNr,maxLoadVoltageTimeStep);
+    fprintf('Minimum load voltage: %g at bus %d, time %d\n',minLoadVoltage,minLoadVoltageBusNr,minLoadVoltageTimeStep);
 
     % fprintf('Minimum voltage difference: %g\n',minLoadVdiff);
     % fprintf('Maximum voltage difference: %g\n',maxLoadVdiff);

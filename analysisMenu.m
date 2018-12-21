81fd3585420c0d455d4c9d0e26773e2cefcedd86
lbs129 <lbs129@hotmail.com>
2018-12-21 15:45:51 +0100

    Fixes and improvements

--- analysisMenu.m
+++ analysisMenu.m
@@ -3,9 +3,10 @@ loopAnalysis=true;
 
 while loopAnalysis
     % Select case
-    caseList={'setinput','Set power to input';...
+    caseList={'resetinput','Reset and analyze input';...
               'changepf','Change power factor (BETA)';...
               'addprod','Add power production (BETA)';...
+              'filterinput','Filter input (moving average)';...
               'settimeline','Set timeline';...
               'sweepcalc','Run sweep calculation';...
               'plotall','Plot all results';...
@@ -25,10 +26,10 @@ while loopAnalysis
     analysisCase=strjoin(caseList(sel,1));
 
     switch analysisCase
-        case 'setinput'
-            % Set power to input
+        case 'resetinput'
+            % Reset and analyze input
             S_ana=S_bus;
-            disp('All bus powers set to input values.');
+            disp('All bus powers reset to input values.');
             if all(all(S_bus(busIsLoad,:)==real(S_bus(busIsLoad,:))))
                 disp('Load input contains only active powers.');
             else
@@ -36,10 +37,10 @@ while loopAnalysis
             end
         case 'changepf'
             % Change power factor
+            if ~exist('S_ana','var'), S_ana = S_bus; end            % Powers for analysis set to inputs
             histogramNodeNumber = input('Enter new power factor: ');
             newPowerFactorLeading = input('Lagging/inductive (0) or leading/capacitive (1)? ');
-            fprintf('\n');
-            if ~exist('S_ana','var'), S_ana = S_bus; end            % Powers for analysis set to inputs
+            %fprintf('\n');
 
             S_ana(busIsLoad,:)=createComplexPower(S_ana(busIsLoad,:),histogramNodeNumber,newPowerFactorLeading);
             fprintf('Power factor for all loads changed to %g',histogramNodeNumber)
@@ -47,7 +48,6 @@ while loopAnalysis
 
         case 'addprod'
             % Add power production
-
             if ~exist('S_ana','var'), S_ana = S_bus; end
             productionAtBus=[118;120];          % Bus nr to add production to
             productionAtTime=70:170;            % Time interval for production
@@ -56,27 +56,37 @@ while loopAnalysis
             S_ana(productionAtBus,productionAtTime)=S_ana(productionAtBus,productionAtTime)...
                 -productionPower;
             fprintf('Added %g p.u. production at node %d\n',[productionPower productionAtBus]');
+        
+        case 'filterinput'
+            % Filter input (moving average)
+            if ~exist('S_ana','var'), S_ana = S_bus; end
+            filterInput;
+            
         case 'settimeline'
+            % Set timeline
             leapYear = input('Leap year?  No (0) or Yes (1): ');
             timeLine=setTimeLine(leapYear);
 
         case 'sweepcalc'
             % Run sweep calculation
-
+            if ~exist('timeLine','var'), disp('Error: Set timeline first'); break; end
             if ~exist('S_ana','var'), S_ana = S_bus; end
             [U_hist,S_hist,nIters]=doSweepCalcs(Z_ser_tot,S_ana,U_bus,connectionBuses,busType,timeLine);
 
 
         case 'plotall'
             % Plot all
+            if ~exist('U_hist','var'), disp('Error: Run or load calculation first.'); break; end
             plotResults(U_hist,S_hist,timeLine,busIsLoad,0);
 
         case 'plotloads'
             % Plot loads only
+            if ~exist('U_hist','var'), disp('Error: Run or load calculation first.'); break; end
             plotResults(U_hist,S_hist,timeLine,busIsLoad,1);
 
         case 'analyzevolt'    
             % Analyze voltages
+            if ~exist('U_hist','var'), disp('Error: Run or load calculation first.'); break; end
             V_anaRes=analyzeVoltage(U_hist,busIsLoad);
 
         case 'plotgridtree'
@@ -85,6 +95,7 @@ while loopAnalysis
             
         case 'plotvhist'
             % Plot voltage histogram
+            if ~exist('U_hist','var'), disp('Error: Run or load calculation first.'); break; end
             plotVoltageHistogram;
 
         case {'cancel'}
--- filterInput.m
+++ filterInput.m
@@ -1,10 +1,13 @@
 % Moving-Average Filter
-windowSize = 5;
+windowSize = input('Set window size for moving average filter: ');
 b = (1/windowSize)*ones(1,windowSize);
 a = 1;
-S_ana = filter(b,a,S_bus,[],2);
-plot(timeLine,abs(S_ana(timeLine)))
-figure
-plot(timeLine,abs(S_bus(timeLine)))
-mean(mean(S_ana,2))
-mean(mean(S_bus,2))
+meanBefore=mean(mean(S_ana,2));
+S_ana = filter(b,a,S_ana,[],2);
+meanAfter=mean(mean(S_ana,2));
+match=meanBefore/meanAfter;
+fprintf('Input data filtered with window size %d. Mean value match: %g.\n',windowSize,match);
+% plot(timeLine,abs(S_ana(timeLine)))
+% figure
+% plot(timeLine,abs(S_bus(timeLine)))
+clear meanPrev meanAfter windowSize b a match

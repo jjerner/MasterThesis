21dac4fddaff67f3a53d5bdb4db81368d2c5a0e1
lbs129 <lbs129@hotmail.com>
2019-01-03 18:01:45 +0100

    Update plotResults.m
    Bug fixes

--- plotResults.m
+++ plotResults.m
@@ -3,27 +3,27 @@ function plotResults(U_hist,S_hist,timeLine,busIsLoad,loadsOnly)
 
     if ~loadsOnly
         figure;
-        plot(timeLine,abs(U_hist));
+        plot(timeLine,abs(U_hist(:,timeLine)));
         title('Voltage (all)');
 
         figure;
-        plot(timeLine,real(S_hist));
+        plot(timeLine,real(S_hist(:,timeLine)));
         title('Active power (all)');
 
         figure;
-        plot(timeLine,imag(S_hist));
+        plot(timeLine,imag(S_hist(:,timeLine)));
         title('Reactive power (all)');
     else
         figure;
-        plot(timeLine,abs(U_hist(busIsLoad,:)));
+        plot(timeLine,abs(U_hist(busIsLoad,timeLine)));
         title('Voltage (loads)');
 
         figure;
-        plot(timeLine,real(S_hist(busIsLoad,:)));
+        plot(timeLine,real(S_hist(busIsLoad,timeLine)));
         title('Active power (loads)');
 
         figure;
-        plot(timeLine,imag(S_hist(busIsLoad,:)));
+        plot(timeLine,imag(S_hist(busIsLoad,timeLine)));
         title('Reactive power (loads)');
     end
 end

4768fae43e237ab4bab943560c8ace80aeb4109b
lbs129 <lbs129@hotmail.com>
2019-01-08 18:59:28 +0100

    Move settings to separate script

---
+++ LoadSettings.m
@@ -0,0 +1,22 @@
+%% Settings
+
+%% Common
+Settings.cableDBPath='data/Ledningsdata.mat';           % Path to cable database file
+Settings.removeHighVoltageBuses = true;                 % Ignore buses before transformer
+Settings.U_t_guess = 1;                                 % Initial transformer voltage guess
+Settings.U_l_guess = 0.97;                              % Initial load voltage guess
+Settings.U_j_guess = 1;                                 % Initial jointconnection voltage guess
+
+%% Location specific
+
+% === Hallonvägen ===
+Settings.location='Hallonvägen';                        % Location name
+Settings.inputDir = 'data/Inputs/T085';                 % Directory for input files
+Settings.gridCableDataPath = 'data/T085 Hallonvagen.xlsx';              % Path to grid cable data file
+Settings.transformerDataPath='data/TransformerData_Hallonvagen.mat';    % Path to transformer data file
+
+% === Amundstorp ===
+% Settings.location='Amundstorp';                         % Location name
+% Settings.inputDir = 'data/Inputs/T317';                 % Directory for input files
+% Settings.gridCableDataPath = 'data/T317 Amundstorp.xlsx';               % Path to grid cable data file
+% Settings.transformerDataPath='data/TransformerData_Amundstorp.mat';     % Path to transformer data file
--- main.m
+++ main.m
@@ -7,23 +7,11 @@ j = 1i;
 addpath('Verification');
 addpath('PV model');
 
-%% Settings
-Settings.location='Hallonvägen';                        % Location name
-Settings.inputDir = 'data/Inputs/T085';                 % Directory for input files
-Settings.cableDBPath='data/Ledningsdata.mat';           % Path to cable database file
-Settings.gridCableDataPath = 'data/T085 Hallonvagen.xlsx';              % Path to grid cable data file
-Settings.transformerDataPath='data/TransformerData_Hallonvagen.mat';    % Path to transformer data file
-Settings.removeHighVoltageBuses = true;                 % Ignore buses before transformer
-Settings.U_t_guess = 1;                                 % initial transformer voltage guess
-Settings.U_l_guess = 0.97;                              % initial load voltage guess
-Settings.U_j_guess = 1;                                 % initial jointconnection voltage guess
-
 %% Run
+LoadSettings;                   % Load settings
 InitializeTransformer;          % Initialize transformer parameters
 InitializeCables;               % Initialize cables
 SetupConnections;               % Set up connection buses, names, type etc.
 SetupArrays;                    % Match cable parameters and connections to matrices
 SetupInput;                     % Read input data from files
 SetupProblem;                   % Set up solver input matrices
-
-

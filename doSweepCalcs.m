37c187c4b7604084d3c15a5656b697b215c8038d
lbs129 <lbs129@hotmail.com>
2019-01-08 17:12:09 +0100

    Update doSweepCalcs.m
    Bug fix

--- doSweepCalcs.m
+++ doSweepCalcs.m
@@ -1,23 +1,26 @@
 function resultSet=doSweepCalcs(Z_ser_tot,S_ana,U_bus,connectionBuses,busType,timeLine)
     S_hist = zeros(size(S_ana,1), length(timeLine));
     U_hist = zeros(size(U_bus,1), length(timeLine));
+    I_hist = zeros(size(connectionsBuses,1), length(timeLine));
     nItersVec=zeros(1,length(timeLine));
     barHandle = waitbar(0, '1', 'Name', 'Sweep calculations');
     for iTime = 1:length(timeLine)
         waitbar(iTime/length(timeLine), barHandle, sprintf('Sweep calculations %d/%d',...
                 iTime, length(timeLine)));
 
-        [S_out,U_out,nIters] = solveFBSM(Z_ser_tot,S_ana(:,timeLine(iTime)),U_bus(:,timeLine(iTime)),...
+        [S_out,U_out,I_out,nIters] = solveFBSM(Z_ser_tot,S_ana(:,timeLine(iTime)),U_bus(:,timeLine(iTime)),...
                               connectionBuses,busType,1000,1e-3,0);
 
         S_hist(:,iTime) = S_out;
         U_hist(:,iTime) = U_out;
+        I_hist(:,iTime) = I_out;
         nItersVec(iTime)=nIters;
     end
     close(barHandle)
     disp('Sweep calculation finished.');
     resultSet.U_hist=U_hist;
     resultSet.S_hist=S_hist;
+    resultSet.I_hist=I_hist;
     resultSet.nItersVec=nItersVec;
     resultSet.timeLine=timeLine;
 end

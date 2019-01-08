dd0b1460df49d5f5063ce2e52a095a8c8fb6398e
lbs129 <lbs129@hotmail.com>
2019-01-08 17:12:03 +0100

    Minisys development

--- minisys.m
+++ minisys.m
@@ -1,3 +1,12 @@
-%clear;
-%load('minisys_real.mat');
-[S_out,U_out,I_out,iter] = solveFBSM(Z_ser, S_in, U_in, connectionBuses, busType,100,1e-3,1)
+j=1i;
+
+for iConnection = 1:length(connectionBuses)
+        startBus = connectionBuses(iConnection,1);
+        endBus = connectionBuses(iConnection,2);
+        disp(['At buses: ', num2str(startBus), ', ', num2str(endBus)]);
+        Z_ser(startBus,endBus) = CableData(iConnection).R + j*CableData(iConnection).X;
+        Z_ser(endBus,startBus) = CableData(iConnection).R + j*CableData(iConnection).X;
+        disp(['Series impedance added from Cable ', num2str(iConnection)])
+end
+
+[S_out,U_out,I_out,iter] = solveFBSM(Z_ser, S_in, U_in, connectionBuses, busType,100,1e-3,0)

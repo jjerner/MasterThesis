bdb15c9a8a73fa340ca63ff6a214642c87d9c38c
Jerner <jonathan.jerner@gmail.com>
2019-01-15 16:03:57 +0100

    Update Transformer.m

--- Verification/Transformer.m
+++ Verification/Transformer.m
@@ -19,21 +19,21 @@ TransformerData.Z2k = TransformerData.R + j*TransformerData.X;
 TransformerData.R2k = 0.4;
 
 % base power and voltages
-TransformerData.S_base      = TransformerData.S_tot/3;
+TransformerData.S_base      = TransformerData.S_tot;
 TransformerData.U_prim_base = TransformerData.U_prim;
-TransformerData.U_sec_base  = TransformerData.U_sec/sqrt(3);
+TransformerData.U_sec_base  = TransformerData.U_sec;
 
 % base impedances
 TransformerData.Z_prim_base = (TransformerData.U_prim_base^2)/TransformerData.S_base;
 TransformerData.Z_sec_base  = (TransformerData.U_sec_base^2)/TransformerData.S_base;
 
 % base currents
-TransformerData.I_prim_base = TransformerData.S_base / TransformerData.U_prim_base;
-TransformerData.I_sec_base  = TransformerData.S_base / TransformerData.U_sec_base;
+TransformerData.I_prim_base = TransformerData.S_base / (sqrt(3)*TransformerData.U_prim_base);
+TransformerData.I_sec_base  = TransformerData.S_base / (sqrt(3)*TransformerData.U_sec_base);
 
 % Impedances
-TransformerData.Z2k_pu      = TransformerData.Z2k;  
-TransformerData.R2k_pu      = TransformerData.R2k;
+TransformerData.Z2k_pu      = TransformerData.Z2k / TransformerData.Z_sec_base;  
+TransformerData.R2k_pu      = TransformerData.R2k / TransformerData.Z_sec_base;
 
 % TransformerData.R0_pu       = TransformerData.R0;
 TransformerData.Z0_pu       = 0;

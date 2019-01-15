6d31c023a51a2e75c2409fd9e5008067966c2dae
lbs129 <lbs129@hotmail.com>
2019-01-15 15:17:23 +0100

    Update findCableLoadLevel.m
    Handles both high and low voltage levels

--- analysis/findCableLoadLevel.m
+++ analysis/findCableLoadLevel.m
@@ -2,9 +2,12 @@
 
 % Cable currents (transformer removed)
 cableCurrents_pu=[resultSet.I_hist(1:Info.addedTransformerBusAtIndex(1)-1,:);
-          resultSet.I_hist(Info.addedTransformerBusAtIndex(1)+1:end,:)]...
-          .*TransformerData.I_sec_base; % [p.u.]
-      
+                  resultSet.I_hist(Info.addedTransformerBusAtIndex(1)+1:end,:)]; % [p.u.]
+connectionLevel_mod=[connectionLevel(1:Info.addedTransformerBusAtIndex(1)-1);
+                     connectionLevel(Info.addedTransformerBusAtIndex(1)+1:end)];
+cableCurrents=zeros(size(cableCurrents_pu));
+cableCurrents(connectionLevel_mod=='H',:)=cableCurrents_pu(connectionLevel_mod=='H',:).*TransformerData.I_prim_base;  % [A]
+cableCurrents(connectionLevel_mod=='L',:)=cableCurrents_pu(connectionLevel_mod=='L',:).*TransformerData.I_sec_base;   % [A]
 
 % Maximum line currents allowed [A]
 maxCableCurrents=cell2mat({CableData.Imax})';
@@ -15,3 +18,7 @@ cableLoadLevels=100*cableCurrents./maxCableCurrents;
 % Plot
 plot(resultSet.timeLine,cableLoadLevels);
 title('Line load in percent');
+xlabel('Timeline');
+ylabel('Load level (I/I_{max}) [%]');
+
+clear connectionLevel_mod

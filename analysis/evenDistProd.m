6463a089b21a9cb5b55e1e99f6edb8222be5679b
Jerner <jonathan.jerner@gmail.com>
2019-01-31 18:26:23 +0100

    Update evenDistProd.m

--- analysis/evenDistProd.m
+++ analysis/evenDistProd.m
@@ -4,7 +4,7 @@ disp('Calculating PV power from model.');
 P_pv=(1/TransformerData.S_base).*PV_model(1,1,1,3)';    % Get PV power from model [p.u.]
 P_pv=P_pv(timeLine);                                    % Set correct timeline
 
-pvFactor = 5;
+pvFactor = 1;
 busNumber = (1:Info.nBuses)';
 loadNumber = busNumber(busIsLoad);
 minAllowed = 0.9;

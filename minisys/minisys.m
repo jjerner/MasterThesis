15ac0997599576fc4a3955c771e22f34267f49e7
lbs129 <lbs129@hotmail.com>
2019-02-07 16:08:36 +0100

    Minisys development

--- minisys/minisys.m
+++ minisys/minisys.m
@@ -1,6 +1,8 @@
 j=1i;
 addpath('..');
 
+shuntCap=0;
+
 for iConnection = 1:size(connectionBuses,1)
         startBus = connectionBuses(iConnection,1);
         endBus = connectionBuses(iConnection,2);
@@ -8,11 +10,16 @@ for iConnection = 1:size(connectionBuses,1)
         Z_ser(startBus,endBus) = CableData(iConnection).R + j*CableData(iConnection).X;
         Z_ser(endBus,startBus) = CableData(iConnection).R + j*CableData(iConnection).X;
         disp(['Series impedance added from Cable ', num2str(iConnection)])
+        if isfield(CableData,'G')
+            Y_shu(startBus,endBus) = CableData(iConnection).G + j*CableData(iConnection).B;
+            Y_shu(endBus,startBus) = CableData(iConnection).G + j*CableData(iConnection).B;
+            disp(['Shunt admittance added from Cable ', num2str(iConnection)])
+        else
+            Y_shu = zeros(size(Z_ser));
+        end
 end
 
-Y_shu = zeros(size(Z_ser));
-
-Results = solveFBSM(Z_ser,Y_shu,S_in, U_in, connectionBuses, busType,100,1e-6,0);
+Results = solveFBSM(Z_ser,Y_shu,S_in, U_in, connectionBuses, busType,100,1e-6,0,shuntCap);
 
 S_out=Results.S_out
 U_out=Results.U_out
--- minisys/minisys_ver.m
+++ minisys/minisys_ver.m
@@ -4,35 +4,25 @@
 sign(real(S_out)).*abs(S_out./(sqrt(3)*U_out))
 I_out(1)-(I_out(2)+I_out(3))
 
-%% Minisys_complex_dual_3p
+%% Minisys_complex_3p
 
 % Known
-U(1)=400;
-I12=7;
-I23=4;
-I24=3;
-Z(1)=1+0.1j;
-Z(2)=1+0.1j;
-Z(3)=1.5+0.1j;
+U_ver(1)=3.2456e+02 + 2.3380e+02i;
+I12=3.7042;
+I23=3.7042;
+Z(1)=1;
+Z(2)=1;
 
 % Calculated
-U12=sqrt(3)*Z1*I12;
-U23=sqrt(3)*Z2*I23;
-U24=sqrt(3)*Z3*I24;
-
-U(2)=U(1)-U12;
-U(3)=U(2)-U23;
-U(4)=U(2)-U24;
-
-S(1)=sqrt(3)*U(1)*I12;
-S(2)=sqrt(3)*U(2)*I12;
-S(3)=sqrt(3)*U(3)*I23;
-S(4)=sqrt(3)*U(4)*I24;
+U12=sqrt(3)*Z(1)*I12;
+U23=sqrt(3)*Z(2)*I23;
+U_ver(2)=U_ver(1)-U12;
+U_ver(3)=U_ver(2)-U23;
 
 %% Minisys_complex_dual_3p
 
 % Known
-U(1)=3.2647e+02 + 2.3113e+02i;
+U_ver(1)=3.2647e+02 + 2.3113e+02i;
 I12=7.5590;
 I23=4.5361;
 I24=3.0228;
@@ -44,19 +34,43 @@ Z(3)=1.5+0.1j;
 U12=sqrt(3)*Z(1)*I12;
 U23=sqrt(3)*Z(2)*I23;
 U24=sqrt(3)*Z(3)*I24;
-U(2)=U(1)-U12;
-U(3)=U(2)-U23;
-U(4)=U(2)-U24;
+U_ver(2)=U_ver(1)-U12;
+U_ver(3)=U_ver(2)-U23;
+U_ver(4)=U_ver(2)-U24;
 
-S(1)=sqrt(3)*U(1)*I12;
-S(2)=sqrt(3)*U(2)*I12;
-S(3)=sqrt(3)*U(3)*I23;
-S(4)=sqrt(3)*U(4)*I24;
+S_ver(1)=sqrt(3)*U_ver(1)*I12;
+S_ver(2)=sqrt(3)*U_ver(2)*I12;
+S_ver(3)=sqrt(3)*U_ver(3)*I23;
+S_ver(4)=sqrt(3)*U_ver(4)*I24;
+
+%% Minisys_complex_dual_3p_neg-reac-pow
+
+% Known
+U_ver(1)=3.2825e+02 - 2.2859e+02i;
+I12=7.5120;
+I23=4.5065;
+I24=3.0055;
+Z(1)=1+0j;
+Z(2)=1+0j;
+Z(3)=1.5+0j;
+
+% Calculated
+U12=sqrt(3)*Z(1)*I12;
+U23=sqrt(3)*Z(2)*I23;
+U24=sqrt(3)*Z(3)*I24;
+U_ver(2)=U_ver(1)-U12;
+U_ver(3)=U_ver(2)-U23;
+U_ver(4)=U_ver(2)-U24;
+
+S_ver(1)=sqrt(3)*U_ver(1)*I12;
+S_ver(2)=sqrt(3)*U_ver(2)*I12;
+S_ver(3)=sqrt(3)*U_ver(3)*I23;
+S_ver(4)=sqrt(3)*U_ver(4)*I24;
 
 %% Minisys_complex_dual_3p_neg
 
 % Known
-U(1)=3.0596e+02 + 2.5767e+02i;
+U_ver(1)=3.0596e+02 + 2.5767e+02i;
 I12=-1.3254;
 I23=-4.2448;
 I24=2.9174;
@@ -68,11 +82,11 @@ Z(3)=1.5+0.1j;
 U12=sqrt(3)*Z(1)*I12;
 U23=sqrt(3)*Z(2)*I23;
 U24=sqrt(3)*Z(3)*I24;
-U(2)=U(1)-U12;
-U(3)=U(2)-U23;
-U(4)=U(2)-U24;
+U_ver(2)=U_ver(1)-U12;
+U_ver(3)=U_ver(2)-U23;
+U_ver(4)=U_ver(2)-U24;
 
-S(1)=sqrt(3)*U(1)*I12;
-S(2)=sqrt(3)*U(2)*I12;
-S(3)=sqrt(3)*U(3)*I23;
-S(4)=sqrt(3)*U(4)*I24;
+S_ver(1)=sqrt(3)*U_ver(1)*I12;
+S_ver(2)=sqrt(3)*U_ver(2)*I12;
+S_ver(3)=sqrt(3)*U_ver(3)*I23;
+S_ver(4)=sqrt(3)*U_ver(4)*I24;

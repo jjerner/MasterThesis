a5e120178a8244f37bbe8fbb7ace701a83a9ca96
Jerner <jonathan.jerner@gmail.com>
2019-03-05 16:22:47 +0100

    fixes

--- analysis/critPlot.m
+++ analysis/critPlot.m
@@ -18,15 +18,15 @@ monthTick2 = [0, 1417, 2881, 4345, 5833, 7297];
 if ~exist('atIter','var')
     structName = inputname(1);
     if strcmp(structName ,'EvenDist')     
-        atIter = 17; % 17 nås gräns
-        % atIter = 26; % 26 är exakt 1 panel per hus
+        %atIter = 17; % 17 nås gräns
+        atIter = 26; % 26 är exakt 1 panel per hus
         
         str = 'Even Dist.';        
         prodkW = max(plotStruct(atIter).PvPowerPerLoad).*TransformerData.S_base./1000;
         prodkW = prodkW * length(loadNumber);
     elseif strcmp(structName, 'SelectDist')
-        atIter = 40; % 28 nås gräns i weak || 40 nås gräns i strong
-        % atIter = 43; % 43 samma total prod som iter 17 i EvenDist (ish)
+        %atIter = 40; % 28 nås gräns i weak || 40 nås gräns i strong
+        atIter = 43; % 43 samma total prod som iter 17 i EvenDist (ish)
         
         str = 'Selected Dist.';
         prodkW = max(plotStruct(atIter).PvPowerPerLoad).*TransformerData.S_base./1000;
@@ -42,42 +42,54 @@ fprintf('Crit bus = %d \n', loadNumber(critload))
 
 % Plot av fallet där kritisk spänning uppnåtts samt fallet utan produktion
 % för eventuell jämförelse
-figure('Position', [170, 180, 1350, 800]);
+figure('Position', [170, 180, 1200, 400]);
 % sgtitle(['Critical Bus, ', num2str(plotStruct(atIter).Critical.maxVoltage.BusNumber),...
 %        ' Produced Power: ', num2str(prodkW), ' kW'])
 sgtitle(['Critical Bus during ', str, ', ', ' Total Power Production: ', num2str(prodkW), ' kW'])
-subplot(2,2,2)
-plot(timeLine, abs(plotStruct(atIter).Results.U_hist(critload,:)).*TransformerData.U_sec_base./sqrt(3))
+subplot(1,2,1)
+plot(timeLine, abs(plotStruct(atIter).Results.U_hist(critload,:)).*TransformerData.U_sec_base./sqrt(3),'b')
 hold on
+plot(timeLine, abs(plotStruct(1).Results.U_hist(critload,:)).*TransformerData.U_sec_base./sqrt(3),'g')
 plot(timeLine, ones(length(timeLine),1).*230*1.1,'r')
 xlim([0,timeLine(end)]);
+ylim([225, 260]);
 set(gca,'xtick',monthTick2,'xticklabel',months2)
 grid on
-title('Voltage with Production')
+title('Voltage')
 ylabel('Voltage (line-to-neutral) [V]')
-subplot(2,2,4)
-plot(timeLine, real(plotStruct(atIter).Results.S_hist(critload,:)).*TransformerData.S_base./1000)
-xlim([0,timeLine(end)]);
-set(gca,'xtick',monthTick2,'xticklabel',months2)
-grid on
-title('Power with Production')
-ylabel('Power [kW]')
-subplot(2,2,1)
-plot(timeLine, abs(plotStruct(1).Results.U_hist(critload,:)).*TransformerData.U_sec_base./sqrt(3))
+legend({'with production';'no production'},'Location', 'southeast')
+
+subplot(1,2,2)
+plot(timeLine, real(plotStruct(atIter).Results.S_hist(critload,:)).*TransformerData.S_base./1000,'b')
 hold on
-plot(timeLine, ones(length(timeLine),1).*230*1.1,'r')
+plot(timeLine, real(plotStruct(1).Results.S_hist(critload,:)).*TransformerData.S_base./1000,'g')
+%plot(timeLine, ones(length(timeLine),1).*0,'r')
 xlim([0,timeLine(end)]);
+ylim([-9, 9])
 set(gca,'xtick',monthTick2,'xticklabel',months2)
 grid on
-title('Voltage without Production')
-ylabel('Voltage (line-to-neutral) [V]')
-subplot(2,2,3)
-plot(timeLine, real(plotStruct(1).Results.S_hist(critload,:)).*TransformerData.S_base./1000)
-title('Power without Production')
+title('Power')
 ylabel('Power [kW]')
-xlim([0,timeLine(end)]);
-set(gca,'xtick',monthTick2,'xticklabel',months2)
-grid on
+legend({'with production';'no production'},'Location', 'southeast')
+
+
+% subplot(2,2,1)
+% plot(timeLine, abs(plotStruct(1).Results.U_hist(critload,:)).*TransformerData.U_sec_base./sqrt(3))
+% hold on
+% plot(timeLine, ones(length(timeLine),1).*230*1.1,'r')
+% xlim([0,timeLine(end)]);
+% set(gca,'xtick',monthTick2,'xticklabel',months2)
+% grid on
+% title('Voltage without Production')
+% ylabel('Voltage (line-to-neutral) [V]')
+% 
+% subplot(2,2,3)
+% plot(timeLine, real(plotStruct(1).Results.S_hist(critload,:)).*TransformerData.S_base./1000)
+% title('Power without Production')
+% ylabel('Power [kW]')
+% xlim([0,timeLine(end)]);
+% set(gca,'xtick',monthTick2,'xticklabel',months2)
+% grid on
 
 
 % % Hitta unika kritiska bussar och sedan plotta spänningsökningen i extrempunkten

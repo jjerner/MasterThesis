2b08e843599812d3bde6470e3398615f5f3f0426
Jerner <jonathan.jerner@gmail.com>
2019-03-11 15:09:56 +0100

    Create gs_strong.m

---
+++ analysis/gs_strong.m
@@ -0,0 +1,36 @@
+% Greedy search to find best-case order when adding PV power
+
+% Bus vectors
+allBuses=1:Info.nBuses;
+loadBuses=allBuses(busIsLoad);
+
+% Result without production, for comparison
+ResultNoProd=doSweepCalcs(Z_ser,Y_shu,S_bus,U_bus,connectionBuses,busType,timeLine);
+
+% Get PV power data
+pvPower=PV_model(1,1,1,3)./TransformerData.S_base;
+pvPower=pvPower(timeLine)';
+
+% At first step
+addedPvPowerAt=[];
+busesToTest=loadBuses;
+
+for iStep=1:length(loadBuses)
+    for iOption=1:length(busesToTest)
+        pvBusesInSweep=[addedPvPowerAt; busesToTest(iOption)];
+        S_greedy=S_bus;
+        S_greedy(pvBusesInSweep,timeLine)=S_greedy(pvBusesInSweep,timeLine)...
+            -repmat(pvPower,size(pvBusesInSweep,1),1);
+        ResultTemp=doSweepCalcs(Z_ser,Y_shu,S_greedy,U_bus,connectionBuses,busType,timeLine,false);
+        diffU=abs(abs(ResultTemp.U_hist(busesToTest,:))-abs(ResultNoProd.U_hist(busesToTest,:)));
+        [rowMinDiff, timeMinDiff] = find(diffU == min(min(diffU)));
+        minDiffUAtBus(iOption) = busesToTest(rowMinDiff(1));
+        minDiffU(iOption) = min(min(diffU));
+    end
+    [minDiffUChoice,minDiffUAtBusChoice]=max(minDiffU);
+    chosenBus=minDiffUAtBus(minDiffUAtBusChoice);
+    busesToTest(find(busesToTest==chosenBus))=[];
+    addedPvPowerAt(iStep,1)=chosenBus;
+    clear maxDiffUAtBus maxDiffU;
+    fprintf('Greedy search: Step %d finished. Chose bus %d.\n',iStep,chosenBus);
+end
